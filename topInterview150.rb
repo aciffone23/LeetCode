b3cd94244a68114861ccdab378dbe67647fc8d2e
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-08-01 13:28:17 -0400

    insert delete getRandom solution

--- topInterview150.rb
+++ topInterview150.rb
@@ -369,3 +369,43 @@ def can_jump(nums)
 
   return goal == 0
 end
+
+class RandomizedSet
+  def initialize()
+      @set = {}
+
+  end
+
+
+=begin
+  :type val: Integer
+  :rtype: Boolean
+=end
+  def insert(val)
+      if !@set[val]
+          @set[val] = true
+          return true
+      end
+      return false
+  end
+
+
+=begin
+  :type val: Integer
+  :rtype: Boolean
+=end
+  def remove(val)
+      if @set.delete(val)
+          return true
+      end
+      return false
+  end
+
+
+=begin
+  :rtype: Integer
+=end
+  def get_random()
+      @set.keys.sample
+  end
+end
